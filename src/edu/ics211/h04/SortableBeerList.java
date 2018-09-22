e72a6cc100b3485e8adc0339df59d840195a2484
soeux <soaeux@gmail.com>
2018-09-21 23:12:57 -1000

    pau for the night. saving this last part.

--- src/edu/ics211/h04/SortableBeerList.java
+++ src/edu/ics211/h04/SortableBeerList.java
@@ -1,12 +1,98 @@
-//package edu.ics211.h04;
-//
-//import edu.ics211.h02.Beer;
-//
-///**
-// * what does it do?
-// *
-// * @author Christian Mancha
-// */
-//public class SortableBeerList<Beer> implements IList211<Beer> {
-//
-//}
+package edu.ics211.h04;
+
+import edu.ics211.h02.Beer;
+
+import java.util.Comparator;
+
+/**
+ * what does it do?
+ *
+ * @author Christian Mancha
+ */
+public class SortableBeerList implements IList211<Beer> {
+    private SortableList<Beer> beerList;
+    private Comparator<Beer> compare;
+
+    public SortableBeerList(Comparator<Beer> compare) {
+        this.compare = compare;
+    }
+
+    // for the implementation, since the code is already written in edu.ics211.h04.SortableList we're just gna call it
+
+    /**
+     * Gets the item at the given index.
+     *
+     * @param index the index.
+     * @return the item at the given index.
+     */
+    @Override
+    public Beer get(int index) {
+        return beerList.get(index);
+    }
+
+    /**
+     * Sets the item at the given index.
+     *
+     * @param index   the index.
+     * @param element the new element to set.
+     * @return the old element at index.
+     */
+    @Override
+    public Beer set(int index, Beer element) {
+        return beerList.set(index, element);
+    }
+
+    /**
+     * Returns the index of the given obj or -1.
+     *
+     * @param obj the object to find.
+     * @return the index of the given obj or -1.
+     */
+    @Override
+    public int indexOf(Object obj) {
+        return beerList.indexOf(obj);
+    }
+
+    /**
+     * Returns the size of this list.
+     *
+     * @return the size of this list.
+     */
+    @Override
+    public int size() {
+        return beerList.size();
+    }
+
+    /**
+     * Adds e to the end of the list.
+     *
+     * @param beer the item to add.
+     * @return true if successful, false otherwise.
+     */
+    @Override
+    public boolean add(Beer beer) {
+        return beerList.add(beer);
+    }
+
+    /**
+     * Adds element to the list at the given index.
+     *
+     * @param index   the index.
+     * @param element the element to add.
+     */
+    @Override
+    public void add(int index, Beer element) {
+        beerList.add(index, element);
+    }
+
+    /**
+     * Removes the element at the given index.
+     *
+     * @param index the index.
+     * @return The element removed from the list.
+     */
+    @Override
+    public Beer remove(int index) {
+        return beerList.remove(index);
+    }
+}
