a8cc4cd7e5836e71da80ab8c01d237a300d9a6f0
soeux <soaeux@gmail.com>
2018-10-18 14:50:00 -1000

    finished implementing Queue211 as a CircularArray

--- src/edu/ics211/h08/CircularArrayQueue.java
+++ src/edu/ics211/h08/CircularArrayQueue.java
@@ -1,16 +1,28 @@
 package edu.ics211.h08;
 
+import java.util.NoSuchElementException;
+
 /**
- * what does it do?
+ * Implements Queue211 as a Circular Array
  *
  * @author Christian Mancha
  */
 public class CircularArrayQueue<E> implements Queue211<E> {
-    // class vars
+    // # credit: this entire implementation came from the "Queue ADT Continued" video on the class website
+    // instance variables
+    private E[] data;
+    private int front;
+    private int back;
+    private int size;
 
+    public CircularArrayQueue() {
+        this.data = (E[]) new Object[10];
+        this.front = 0;
+        this.back = data.length - 1;
+        this.size = 0;
+    }
 
     // Queue211 implementation
-
     /**
      * Adds e to the end of the queue. May throw an IllegalStateException if the queue is full.
      *
@@ -19,7 +31,34 @@ public class CircularArrayQueue<E> implements Queue211<E> {
      */
     @Override
     public boolean add(E e) {
-        return false;
+        // check if data isn't full
+        if (size == data.length) {
+            reallocate();
+        }
+
+        // updates back + makes it circular
+        back = (back + 1) % data.length;
+
+        // add e + increment size + return true
+        data[back] = e;
+        size++;
+        return true;
+    }
+
+    /**
+     * Adds e to the end of the queue. Returns false if the queue is full.
+     *
+     * @param e the item to add.
+     * @return true if successful, false otherwise.
+     */
+    @Override
+    public boolean offer(E e) {
+        // add() without the exception
+        try {
+            return add(e);
+        } catch (IllegalStateException e) {
+            return false;
+        }
     }
 
     /**
@@ -31,18 +70,13 @@ public class CircularArrayQueue<E> implements Queue211<E> {
      */
     @Override
     public E element() {
-        return null;
-    }
+        // check if there's anything in data
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
 
-    /**
-     * Adds e to the end of the queue. Returns false if the queue is full.
-     *
-     * @param e the item to add.
-     * @return true if successful, false otherwise.
-     */
-    @Override
-    public boolean offer(E e) {
-        return false;
+        // if we're here, then there's stuff in data and we can return the first element
+        return data[front];
     }
 
     /**
@@ -52,17 +86,12 @@ public class CircularArrayQueue<E> implements Queue211<E> {
      */
     @Override
     public E peek() {
-        return null;
-    }
-
-    /**
-     * Removes and returns the front of this queue.
-     *
-     * @return The front of this queue, removing that item.
-     */
-    @Override
-    public E poll() {
-        return null;
+        // element() without the exception
+        try {
+            return element();
+        } catch (NoSuchElementException) {
+            return null;
+        }
     }
 
     /**
@@ -72,7 +101,31 @@ public class CircularArrayQueue<E> implements Queue211<E> {
      */
     @Override
     public E remove() {
-        return null;
+        // check if there's stuff in data
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+
+        // remember the old value + update front + decrement size + return value
+        E value = data[front];
+        front = (front + 1) % data.length;
+        size--;
+        return value;
+    }
+
+    /**
+     * Removes and returns the front of this queue.
+     *
+     * @return The front of this queue, removing that item.
+     */
+    @Override
+    public E poll() {
+        // remove() without the exception
+        try {
+            return remove();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
     }
 
     /**
@@ -82,6 +135,26 @@ public class CircularArrayQueue<E> implements Queue211<E> {
      */
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    /**
+     * Doubles capacity of data and reallocates the data
+     */
+    private void reallocate() {
+        E[] newData = (E[]) new Object[2 * data.length];
+
+        // front can be anywhere in the array at this point
+        // we have to follow it circularly in order to copy the order to the new array
+        int j = front;
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[j];
+            j = (j + 1) % data.length;
+        }
+
+        // reset front + set back size - 1 + set newData to data
+        front = 0;
+        back = size - 1;
+        data = newData;
     }
 }
